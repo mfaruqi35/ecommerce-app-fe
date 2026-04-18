c8f69f10f555784dba5e46241ed21774a8bcc059
mfaruqi35 <muhammad.faruqi35@gmail.com>
2026-04-18 10:49:57 +0700

    add components etc

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,4 +1,3 @@
-// src/context/CartContext.jsx 
 import { createContext, useContext, useReducer } from 'react';
  
 // Initial state 
@@ -51,40 +50,43 @@ function cartReducer(state, action) {
  
     case 'CLEAR_CART': 
       return initialState;
-    default:
-        return state;
-    }
+     default: 
+      return state; 
+  } 
 }
-
-// Create context 
-const CartContext = createContext(initialState);
-
-// Cart provider component 
-export function CartProvider({ children }) { 
-  const [state, dispatch] = useReducer(cartReducer, initialState); 
  
-  // Action functions 
+// Create context 
+const CartContext = createContext();
+ 
+// Provider component 
+export function CartProvider({ children }) { 
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+ 
   const addItem = (product) => { 
     dispatch({ type: 'ADD_ITEM', payload: product }); 
-  }; 
+  };
  
   const removeItem = (productId) => { 
     dispatch({ type: 'REMOVE_ITEM', payload: productId }); 
-  }; 
+  };
  
   const clearCart = () => { 
     dispatch({ type: 'CLEAR_CART' }); 
-  }; 
+  };
  
-  return <CartContext.Provider value={{...state, addItem, removeItem, clearCart}}>{children}</CartContext.Provider>; 
+  return ( 
+    <CartContext.Provider value={{ ...state, addItem, removeItem, clearCart }}> 
+      {children} 
+    </CartContext.Provider> 
+  ); 
 }
-
-// Custom hook to use the cart context 
+ 
+// Custom hook untuk mengakses cart context 
 export function useCart() { 
   const context = useContext(CartContext); 
-  if (!context) {
-    throw new Error('useCart must be used within a CartProvider');
-  }
-  return context;
+  if (!context) { 
+    throw new Error('useCart must be used within a CartProvider'); 
+  } 
+  return context; 
 }
  
